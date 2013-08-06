d327176cf0f6a34d99f42790376b7d4da2876d55
cclim <austincclim@gmail.com>
2013-08-06 23:58:32 +0800

    6AUG

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130724082535) do
+ActiveRecord::Schema.define(version: 20130731131849) do
 
   create_table "clients", force: true do |t|
     t.string   "name"
