ab94bbde38af030835dd6c828510829f6c288286
rubykaikean <khongkk89@gmail.com>
2013-10-09 18:44:07 +0800

    download role

--- app/models/price_control.rb
+++ app/models/price_control.rb
@@ -1,6 +1,6 @@
 class PriceControl < ActiveRecord::Base
 
-	#validates :pp_date, :presence => true
+	validates :pp_date, :client_id ,:presence => true
 	has_many :price_control_items, :dependent => :destroy
 
 	 belongs_to :trade_company
