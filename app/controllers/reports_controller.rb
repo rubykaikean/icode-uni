476a46b8b72711d2a0dcbfc7b7d4e25ce70b89cf
rubykaikean <khongkk89@gmail.com>
2014-05-07 16:59:04 +0800

    done estimation item report layout

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -19,13 +19,13 @@ class ReportsController < ApplicationController
         # render :text => params[:estimation_ids].to_json
         @estimation = Estimation.find(params[:estimation_ids])
         #render :text => @estimation.to_json
-        # html = render_to_string(:layout => false , :action => "pdf_estimation_report.html.erb")
-        #   @kit = PDFKit.new(html)
-        #   @kit.stylesheets << "#{Rails.root.to_s}/app/assets/stylesheets/bootstrap.css"
-        #   send_data(@kit.to_pdf, :filename => "pdf_estimation_report.pdf",
-        #                           :type => 'application/pdf',
-        #                           :disposition => "attachement")
-        render :text => @estimation.to_json
+        html = render_to_string(:layout => false , :action => "pdf_estimation_report.html.erb")
+          @kit = PDFKit.new(html)
+          @kit.stylesheets << "#{Rails.root.to_s}/app/assets/stylesheets/bootstrap.css"
+          send_data(@kit.to_pdf, :filename => "pdf_estimation_report.pdf",
+                                  :type => 'application/pdf',
+                                  :disposition => "attachement")
+        #render :text => @estimation.to_json
       end
     elsif params[:commit] == "Show"
       if params[:estimation_ids].present?
